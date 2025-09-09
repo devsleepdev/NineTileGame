f0cd351aa9b346fc8a85dd60c78209838554be88
BMT DevSleepDev <boonyanit.dev@gmail.com>
2025-09-09 10:09:15 +0700

    Start working with touche features

--- script.js
+++ script.js
@@ -128,3 +128,54 @@ function moveRight()
 }
 
 
+// Detech touches -----------------------------------------------------------
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* right swipe */ 
+            moveRight()
+        } else {
+            /* left swipe */
+            moveLeft()
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* down swipe */ 
+            moveDown()
+        } else { 
+            /* up swipe */
+            moveUp()
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+// Done detect touches -----------------------------------------------
